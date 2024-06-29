5d4c910308b22d64c45462e61fc58c10761152d3
Shakirth Anisha <144751453+shakirth-anisha@users.noreply.github.com>
2024-06-29 11:58:31 +0530

    Update RPS3.py

--- RPS3.py
+++ RPS3.py
@@ -1,12 +1,12 @@
 #Just importing functions
-from tkinter import *
-from tkinter import ttk
+import os
+from tkinter import Tk, PhotoImage, Button, Label
 from tkinter import messagebox
 import random
 
 #Starting out the tkinter stuff
 rps = Tk()
-rps.geometry("1000x800")
+rps.geometry("1000x925")
 rps["background"]="#B9B2DC"
 rps.title("Rock Paper Scissors")
 
@@ -14,95 +14,131 @@ rps.title("Rock Paper Scissors")
 c_value = {
     0:"rock",
     1:"paper",
-    2:"scissors"
-}
+    2:"scissors"}
 
-#________________Importing images________________
-rock_img = PhotoImage(file="Rock.png")
-paper_img = PhotoImage(file="Paper.png")
+#________________Importing images and Declaring Values________________
+rock_img = PhotoImage(file=f"{os.getcwd()}/pics/Rock.png")
+paper_img = PhotoImage(file=f"{os.getcwd()}/pics/Paper.png")
 #DONE____add in the next line later
-scissors_img = PhotoImage(file="Scissors.png")
-rock_txt = PhotoImage(file="Rock_text.png")
-paper_txt = PhotoImage(file="Paper_text.png")
-scissors_txt = PhotoImage(file="Scissors_text.png")
+scissors_img = PhotoImage(file=f"{os.getcwd()}/pics/Scissors.png")
+rock_txt = PhotoImage(file=f"{os.getcwd()}/pics/Rock_text.png")
+paper_txt = PhotoImage(file=f"{os.getcwd()}/pics/Paper_text.png")
+scissors_txt = PhotoImage(file=f"{os.getcwd()}/pics/Scissors_text.png")
+play_again = PhotoImage(file=f"{os.getcwd()}/pics/Play_Again_text.png")
+
+Win = "AYOOOOOOOOOOOO You Won!!! :("
+Draw = "Draw???? Well That was anticlimatic :|"
+Lose = "HAHAHAHAHAHAHAHA LOSER!!!! :)"
+
+
+player_score = 0
+pc_score = 0
 #______Cancelled______
 #playagain = PhotoImage(file="Play_Again_text.png")
 
 #______Cancelled______
-'''#________________Disabling buttons after match________________
-def disable():
+#________________Disabling buttons after match________________
+'''def disable():
     rock_btn.config(state="disabled")
     paper_btn.config(state="disabled")
-    scissors_btn.config(state="disabled")'''
+    scissors_btn.config(state="disabled")
+    pc_score = 0
+    player_score = 0'''
     
 ##________________Declaring win/lose conditions________________
-#had to place this individually in each condition as it doesnt change when u hit lay againif its outisde
+#had to place this individually in each condition as it doesnt change when u play again if its outisde
 # c_choice = c_value[random.randint(0,2)]
 
 def p_rock():
+    global pc_score, player_score
     c_choice = c_value[random.randint(0,2)]
     
     if c_choice == "rock":
         pc_choice.config(image=rock_img)
-        result = "Draw???? Well That was anticlimatic :|"
+        result = Draw
         
     elif c_choice == "paper":
         pc_choice.config(image=paper_img)
-        result = "HAHAHAHAHAHAHHAHA LOSER!!!! :)"
+        result = Lose
+        pc_score += 1
     else:
         pc_choice.config(image=scissors_img)
-        result = "AYOOOOOOOOOOOO You Won!!! :("
+        result = Win
+        player_score += 1
         
     result_box.config(text=result)
     your_choice.config(image=rock_img)
+    player_score_box.config(text=f"Player Score: {player_score}")
+    pc_score_box.config(text=f"Computer Score: {pc_score}")
 #disable()
         
 def p_paper():
+    global pc_score, player_score
     c_choice = c_value[random.randint(0,2)]
     
     if c_choice == "rock":
-        result = "AYOOOOOOOOOOOO You Won!!! :("
+        result = Win
+        player_score += 1
         pc_choice.config(image=rock_img)
     elif c_choice == "paper":
         pc_choice.config(image=paper_img)
-        result = "Draw???? Well That was anticlimatic :|"
+        result = Draw
     else:
         pc_choice.config(image=scissors_img)
-        result = "HAHAHAHAHAAHHAHA LOSER!!!! :)"
+        result = Lose
+        pc_score += 1
     
     result_box.config(text=result) 
-    your_choice.config(image=paper_img)  
+    your_choice.config(image=paper_img)
+    player_score_box.config(text=f"Player Score: {player_score}")
+    pc_score_box.config(text=f"Computer Score: {pc_score}")  
 #disable()
         
 def p_scissors():
+    global pc_score, player_score
     c_choice = c_value[random.randint(0,2)]
     
     if c_choice == "rock":
         pc_choice.config(image=rock_img)
-        result = "HAHAHAHAHAHAHAHA LOSER!!!! :)"
+        result = Lose
+        pc_score += 1
     elif c_choice == "paper":
         pc_choice.config(image=paper_img)
-        result = "AYOOOOOOOOOOOO You Won!!! :("
+        result = Win
+        player_score += 1
     else:
         pc_choice.config(image=scissors_img)
-        result = "Draw???? Well That was anticlimatic :|"
+        result = Draw
         
     result_box.config(text=result)
     your_choice.config(image=scissors_img)
+    player_score_box.config(text=f"Player Score: {player_score}")
+    pc_score_box.config(text=f"Computer Score: {pc_score}")
 #disable()
         
 #Replay button
 def reset():
+    global pc_score, player_score
     rock_btn.config(state="active")
     paper_btn.config(state="active")
     scissors_btn.config(state="active")
     result_box.config(text="")
     pc_choice.config(image="")
     your_choice.config(image="")
+    pc_score = 0
+    player_score = 0
+    player_score_box.config(text=f"Player Score: {player_score}")
+    pc_score_box.config(text=f"Computer Score: {pc_score}")
+    
 ##Trying out stuff
 #Just edit out this def function later
-'''def itworks():
-    msg = messagebox.showinfo('Yoooo', 'It Works!!!!')'''
+def itworks():
+    msg = messagebox.showinfo('Yoooo', 'It Works!!!!')
+    
+# def end_game():
+#     global pc_score, player_score
+#     if pc_score>player_score:
+#         end = messagebox.showinfo("You Lost :(")
 
 ##________________CREATING BUTTONS/LABELS________________
 #Player buttons
@@ -120,6 +156,9 @@ scissors_btn = Button(rps, image=scissors_img, command=p_scissors, borderwidth=0
 scissors_btn.place(x=700,y=450)
 scissors_text = Button(rps, image=scissors_txt, command=p_scissors, borderwidth=0, highlightthickness=0)
 scissors_text.place(x=700, y=675)
+#
+play_again_btn = Button(rps, image=play_again, command=reset, borderwidth=0, highlightthickness=0)
+play_again_btn.place(x=300, y=800)
 
 #Choices Made
 pc_text = Label(text="Computer's Choice", font=("Helvatica bold", 25), bg="#B9B2DC")
@@ -138,22 +177,38 @@ your_choice.place(x=100, y=100)
 #Win/Lose Text box
 result_box = Label(font=("Helvatica bold", 50), bg="#B9B2DC")
 result_box.place(x=100, y=350)
-#______Cancelled______
+
+#Score boxes
+player_score_box = Label(text="Player Score",font=("Helvatica bold", 20), bg="#B9B2DC")
+player_score_box.place(x=125, y=325)
+pc_score_box = Label(text="Computer Score", font=("Helvatica bold", 20), bg="#B9B2DC")
+pc_score_box.place(x=725, y=325)
+
+#______________________CANCELLED______________________
 #Play again button
 #Add image for this shit
 '''play_again = Button(image=playagain, command=reset, borderwidth=0, highlightthickness=0)
-play_again.place(x=300, y=750)'''
+play_again.place(x=300, y=750)
 
-#________________DISPLAYING SCORES________________
+
+#Found a better way to display scores
 #use cget and work with that somehow
-
-
+player_score = 0
+pc_score = 0
+def scores():
+    if res == Win:
+        player_score = player_score + 1
+        
+    elif res == Lose:
+        pc_score = pc_score + 1
+'''
 ##OUTLINE
 #______DONE______Computer option written on the top along with the pic of the chosen option.
 #______DONE______A "vs" screen in middle with Your and Computer option pics displayed
 #(Maybe try animation delay before the upper one)
 #______DONE______The win/lose message under that
-#Get and display scores
+#______DONE______Get and display scores
 #______Cancelled______Reset button under that
+#(Restared wrt scores) Reset button under that
 
 rps.mainloop()
